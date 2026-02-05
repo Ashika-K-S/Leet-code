dad29af2a805dff5628867aaccf4197cb327e846
Ashika-K-S <ashikaksuresh@gmail.com>
2026-02-05 11:44:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (91.04%) - LeetSync

---
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def averageValue(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total=0;
+        count=0;
+        for num in nums:
+                if num%6==0:
+                    total+=num
+                    count+=1
+        if count==0:
+            return 0
+        return total // count
